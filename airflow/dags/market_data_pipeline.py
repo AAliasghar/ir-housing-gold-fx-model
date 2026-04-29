9c248d3044d70a668fee50f3990a08ece3378e4b
AAliasghar <a.asgharinia@yahoo.com>
2026-04-29 10:32:17 +0200

    create gold table in Postgres

--- airflow/dags/market_data_pipeline.py
+++ airflow/dags/market_data_pipeline.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 
 # This is a simple test function
@@ -9,6 +10,17 @@ def hello_airflow():
 
 
 # Define the "Brain" of the DAG
+# with DAG(
+#     dag_id="market_data_scraper",
+#     start_date=datetime(2024, 1, 1),
+#     schedule_interval="@daily",
+#     catchup=False,
+# ) as dag:
+
+#     test_task = PythonOperator(
+#         task_id="check_connection", python_callable=hello_airflow
+#     )
+
 with DAG(
     dag_id="market_data_scraper",
     start_date=datetime(2024, 1, 1),
@@ -16,6 +28,17 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    test_task = PythonOperator(
-        task_id="check_connection", python_callable=hello_airflow
+    # Task 1: Create a table for Gold prices
+    create_table = PostgresOperator(
+        task_id="create_gold_table",
+        postgres_conn_id="postgres_default",  # We will set this in the UI next
+        sql="""
+        CREATE SCHEMA IF NOT EXISTS raw;
+        CREATE TABLE IF NOT EXISTS raw.gold_prices (
+            id SERIAL PRIMARY KEY,
+            date TIMESTAMP,
+            price FLOAT,
+            currency TEXT
+);
+        """,
     )
